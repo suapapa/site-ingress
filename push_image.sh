0f0e6d16f5f7fb25d587d04a71a4105ad2e5cf8e
suapapa <Homin Lee>
2022-09-05 17:03:45 +0900

    rename script for push image

--- push_image.sh
+++ push_image.sh
@@ -2,6 +2,3 @@
 IMAGE_TAG=gcr.io/homin-dev/ingress:latest 
 docker buildx build --platform linux/amd64 -t $IMAGE_TAG .
 docker push $IMAGE_TAG
-# kubectl delete -f ingress-deploy.yaml
-# sleep 1
-# kubectl apply -f ingress-deploy.yaml
