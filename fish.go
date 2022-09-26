c927b06198bbd91b0e560bf50b186030b68a40c3
Homin Lee <ff4500@gmail.com>
2022-09-27 08:35:39 +0900

    Add more fish script

--- fish.go
+++ fish.go
@@ -8,22 +8,29 @@ import (
 var (
 	movieLines = []*MovieLine{
 		{
+			Movie:     "Matrix",
 			Line:      "Neo, sooner or later you’re going to realize, just as I did, that there’s a difference between knowing the path and walking the path.",
 			Character: "Morpheus",
 		},
 		{
+			Movie:     "Forrest Gump",
 			Line:      "Life is like a box of chocolates. You never know what you're gonna get.",
 			Character: "Forrest Gump",
+		},
+		{
+			Movie:     "Dune (2021)",
+			Line:      "A great man doesn't seek to lead, he's called to it",
+			Character: "Leto",
 		},
 	}
 )
 
 type MovieLine struct {
-	Line, Character string
+	Movie, Line, Character string
 }
 
 func (ml *MovieLine) String() string {
-	return fmt.Sprintf("“%s” ― %s", ml.Line, ml.Character)
+	return fmt.Sprintf("“%s” - %s; %s", ml.Line, ml.Character, ml.Movie)
 }
 
 func randLine() string {
