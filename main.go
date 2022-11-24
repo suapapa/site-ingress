0800e42f35c7cd560d5bee99c4e9691b1814ede9
Homin Lee <ff4500@gmail.com>
2022-11-24 12:31:34 +0900

    update jaeger tracer ep

--- main.go
+++ main.go
@@ -40,8 +40,7 @@ func main() {
 	flag.BoolVar(&debug, "d", false, "print debug logs")
 	flag.Parse()
 
-	// tp, err := tracerProvider("http://simplest-collector.default.svc.cluster.local:14268")
-	tp, err := tracerProvider("http://simplest-collector.default.svc.cluster.local:4318")
+	tp, err := tracerProvider("http://simplest-collector.default.svc.cluster.local:14268/api/traces")
 	if err != nil {
 		log.Fatal(err)
 	}
