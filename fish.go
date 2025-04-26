642859a8e812e20998a1575dfccd4725b97c41f5
Homin Lee <ff4500@gmail.com>
2025-04-26 09:36:10 +0900

    fix fish

--- fish.go
+++ fish.go
@@ -4,39 +4,49 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+
+	"github.com/goccy/go-yaml"
+)
+
+const (
+	movieLinesStr = `
+- movie: "Matrix"
+  line: "Neo, sooner or later you’re going to realize, just as I did, that there’s a difference between knowing the path and walking the path."
+  character: "Morpheus"
+- movie: "Forrest Gump"
+  line: "Life is like a box of chocolates. You never know what you're gonna get."
+  character: "Forrest Gump"
+- movie: "Dune (2021)"
+  line: "A great man doesn't seek to lead, he's called to it."
+  character: "Leto"
+- movie: "Interstella"
+  line: "We’ll find a way, we always have."
+  character: "Cooper"
+- movie: "The Lord of the Rings: The Fellowship of the Ring"
+  line: "All we have to decide is what to do with the time that is given us."
+  character: "Gandalf"
+- movie: "Star Wars: Episode V - The Empire Strikes Back"
+  line: "Do or do not. There is no try."
+  character: "Yoda"
+`
+)
+
+var (
+	r          *rand.Rand
+	movieLines []*MovieLine
 )
 
 func init() {
-	rand.Seed(time.Now().Unix())
+	r = rand.New(rand.NewSource(time.Now().UnixNano()))
+	if err := yaml.Unmarshal([]byte(movieLinesStr), &movieLines); err != nil {
+		panic(err)
+	}
 }
 
-var (
-	movieLines = []*MovieLine{
-		{
-			Movie:     "Matrix",
-			Line:      "Neo, sooner or later you’re going to realize, just as I did, that there’s a difference between knowing the path and walking the path.",
-			Character: "Morpheus",
-		},
-		{
-			Movie:     "Forrest Gump",
-			Line:      "Life is like a box of chocolates. You never know what you're gonna get.",
-			Character: "Forrest Gump",
-		},
-		{
-			Movie:     "Dune (2021)",
-			Line:      "A great man doesn't seek to lead, he's called to it.",
-			Character: "Leto",
-		},
-		{
-			Movie:     "Interstella",
-			Line:      "We’ll find a way, we always have.",
-			Character: "Cooper",
-		},
-	}
-)
-
 type MovieLine struct {
-	Movie, Line, Character string
+	Movie     string `yaml:"movie"`
+	Line      string `yaml:"line"`
+	Character string `yaml:"character"`
 }
 
 func (ml *MovieLine) String() string {
@@ -44,6 +54,6 @@ func (ml *MovieLine) String() string {
 }
 
 func randLine() string {
-	i := rand.Intn(len(movieLines))
+	i := r.Intn(len(movieLines))
 	return movieLines[i].String()
 }
